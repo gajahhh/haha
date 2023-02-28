e15472974cff022df01e2759ce99dc04cd5ab693
gajahhh <126311312+gajahhh@users.noreply.github.com>
2023-02-28 22:46:10 +0700

    v

--- login.php
+++ login.php
@@ -1,5 +1,11 @@
 <?= validation_errors()?>
-<form class="row g-3" method="POST" action="<?=base_url('masyarakat/login')?>">
+
+<?php 
+  if (!empty($error)){
+    echo $error;
+  }
+?>
+<form class="row g-3" method="POST" action="<?=base_url('petugas/login')?>">
   <div class="col-md-12">
     <label for="username" class="form-label">Username</label>
     <input type="text" class="form-control" id="username" name="username" required>
